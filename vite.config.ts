8573758d4490c779df37b2c7333172615b57e3e7
Gerard Saez Sanchez <mynamegerard@gmail.com>
2022-11-13 23:30:43 +0100

    Change vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gerardcv/',
+  base: '/mynameg3rard.github.io/',
   server: {
     host: true,
     watch: {
